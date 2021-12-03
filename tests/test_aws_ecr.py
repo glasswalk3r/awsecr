ff156548cfd5323586104e4fc32b83c44d1ba991
Alceu Rodrigues de Freitas Junior <arfreitas@cpan.org>
2021-12-02 21:07:41 -0300

    feat: increated testing coverage

--- tests/test_aws_ecr.py
+++ tests/test_aws_ecr.py
@@ -3,32 +3,47 @@ import pytest
 import inspect
 from mypy_boto3_sts.type_defs import GetCallerIdentityResponseTypeDef
 
-from awsecr.awsecr import ECRRepos, MissingAWSEnvVar, InvalidPayload, account_info
+from awsecr.awsecr import (
+    ECRRepos,
+    MissingAWSEnvVar,
+    InvalidPayload,
+    account_info
+)
 
 
 class AwsStsStub:
     account = '012345678910'
     user = 'foobar'
 
-    def get_caller_identity(self) -> GetCallerIdentityResponseTypeDef:
+    def __init__(self):
         email = 'arfreitas@cpan.org'
         arn = f'arn:aws:sts::{self.account}:assumed-role/{self.user}/{email}'
-        return {
-                'Account': self.account,
-                'Arn': arn,
-                'ResponseMetadata': {
-                    'HTTPHeaders': {
-                        'content-length': '516',
-                        'content-type': 'text/xml',
-                        'date': 'Thu, 02 Dec 2021 22:27:26 GMT',
-                        'x-amzn-requestid': 'bdad68ca-001e-435b-9215-9ffdf8c29'
-                    },
-                    'HTTPStatusCode': 200,
-                    'RequestId': 'bdad68ca-001e-435b-9215-9ff5d9df8c29',
-                    'RetryAttempts': 0,
-                    'HostId': ''
-                },
-                'UserId': f'AROAQS8CASQSLK3MZM2GM:{email}'}
+        self.payload = {
+                        'Account': self.account,
+                        'Arn': arn,
+                        'ResponseMetadata': {
+                            'HTTPHeaders': {
+                                'content-length': '516',
+                                'content-type': 'text/xml',
+                                'date': 'Thu, 02 Dec 2021 22:27:26 GMT',
+                                'x-amzn-requestid': 'bdad68ca-001e-435b-df8c29'
+                            },
+                            'HTTPStatusCode': 200,
+                            'RequestId': 'bdad68ca-001e-435b-9215-9ff5d9df8c2',
+                            'RetryAttempts': 0,
+                            'HostId': ''
+                        },
+                        'UserId': f'AROAQS8CASQSLK3MZM2GM:{email}'}
+
+    def get_caller_identity(self) -> GetCallerIdentityResponseTypeDef:
+        return self.payload
+
+
+@pytest.fixture
+def broken_sts_client():
+    client = AwsStsStub()
+    client.payload.pop('Account')
+    return client
 
 
 def test_aws_account_id():
@@ -36,6 +51,13 @@ def test_aws_account_id():
     assert result.__class__.__name__ == 'tuple'
     assert result[0] == AwsStsStub.account
     assert result[1] == AwsStsStub.user
+
+
+def test_aws_account_id_exception(broken_sts_client):
+    with pytest.raises(InvalidPayload) as excinfo:
+        account_info(broken_sts_client)
+
+    assert 'Account' in str(excinfo.value)
 
 
 def test_ecr_repos_exceptions():
