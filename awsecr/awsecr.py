5784fa1714fc3fa5e5da914e6bc090796ceb8f36
Alceu Rodrigues de Freitas Junior <arfreitas@cpan.org>
2021-12-05 12:50:24 -0300

    feat: image size support

--- awsecr/awsecr.py
+++ awsecr/awsecr.py
@@ -95,27 +95,33 @@ class ECRImage():
                  registry: str,
                  repository: str,
                  image: ImageDetailTypeDef):
-        self.name: str = f"{registry}/{repository}:{image['imageTags'][0]}"
-        self.status: str = image['imageScanStatus']['status']
+
         findings: Dict[Union[Literal['CRITICAL'], Literal['HIGH'],
                              Literal['INFORMATIONAL'], Literal['LOW'],
                              Literal['MEDIUM'], Literal['UNDEFINED']], int]
 
         try:
+            self.name: str = f"{registry}/{repository}:{image['imageTags'][0]}"
+            self.status: str = image['imageScanStatus']['status']
             summary = image['imageScanFindingsSummary']
             findings = summary['findingSeverityCounts']
+            self.size: int = image['imageSizeInBytes']
         except KeyError as e:
-            print(f'Missing image scanning {e}', sys.stderr)
+            print(f'Missing image scanning {e} information', sys.stderr)
             findings = {'UNDEFINED': 0}
 
         self.vulnerabilities: int = sum(findings.values())
 
     def to_list(self) -> List[str]:
-        return [self.name, self.status, str(self.vulnerabilities)]
+        return [self.name, self.status, '{:.4n}'.format(self.size_in_mb()),
+                str(self.vulnerabilities)]
+
+    def size_in_mb(self):
+        return self.size / (1024 * 1000)
 
     @staticmethod
     def fields() -> List[str]:
-        return ['Image', 'Scan status', 'Vulnerabilities']
+        return ['Image', 'Scan status', 'Size (MB)', 'Vulnerabilities']
 
 
 def list_ecr(account_id: str,
