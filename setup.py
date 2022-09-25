ddeab2c00c82b1395597e9112e7756f8f513ad30
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2022-09-25 15:32:07 -0300

    feat: python 3.10

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     description="Easy interaction with AWS ECR from a CLI",
     entry_points={
